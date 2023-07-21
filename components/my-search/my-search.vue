df2a31dbbb08090e4e3a497340a2dbe7bdf8967e
xy <3196925364@qq.com>
2023-07-21 11:48:00 +0800

    完成了搜索功能的开发

---
+++ components/my-search/my-search.vue
@@ -0,0 +1,58 @@
+<template>
+  <view class="my-search-container" :style="{'background-color':bgcolor}" @click="searchHandler">
+    <view class="my-search-box" :style="{'border-radius':radius+'px'}">
+      <!-- 使用uni-ui提供的图标组件 -->
+      <uni-icons type="search" size="17"></uni-icons>
+      <text class="placeholder">搜索</text>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"my-search",
+    props:{
+      bgcolor:{
+        type:String,
+        default:'#C00000'
+      },
+      radius:{
+        type:Number,
+        default:18
+      }
+    },
+    data() {
+      return {
+        
+      };
+    },
+    methods:{
+      searchHandler(){
+        this.$emit('click')
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.my-search-container{
+  height: 50px;
+  // background-color: #C00000;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  .my-search-box{
+    height: 36px;
+    background-color: #FFFFFF;
+    // border-radius: 18px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .placeholder{
+      font-size: 15px;
+      margin-left: 5px;
+    }
+  }
+}
+</style>
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,9 @@
 <template>
   <view>
+    <!-- 自定义搜索组件 -->
+    <view class="search-box">
+      <my-search @click="gotoSearch"></my-search>
+    </view>
     <!-- 轮播图区域  -->
     <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
       <swiper-item v-for="(item,index) in swiperList" :key=index>
@@ -91,6 +95,11 @@
        })
      })
      this.floorList=res.message
+    },
+    gotoSearch(){
+      uni.navigateTo({
+        url:'/subpkg/search/search'
+      })
     }
     },
   }
@@ -126,5 +135,10 @@ swiper{
   display: flex;
   flex-wrap:wrap;
   justify-content: space-around;
+}
+.search-box{
+  position: sticky;
+  top: 0;
+  z-index: 999;
 }
 </style>
